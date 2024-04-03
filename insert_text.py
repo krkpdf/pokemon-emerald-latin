a9674d3a06ab1939b77b17dfc2e27e50e4e47c9a
krk <pedro.salgado@tuta.io>
2024-04-03 10:49:09 +0200

    version: 2024-04-03

--- insert_text.py
+++ insert_text.py
@@ -10,8 +10,8 @@ j = 0
 
 script_dir = os.path.dirname(__file__)
 
-folder_path_a = "..\\pokeemerald-master-translated\\" # output folder
-folder_path_b = ".\\text\\"                           # input folder
+folder_path_a = os.path.join("..", "pokeemerald-master-translated") + os.sep # output folder
+folder_path_b = os.path.join(".", "text") + os.sep                           # input folder
 
 #print(folder_path_a, folder_path_b)
 
@@ -35,57 +35,60 @@ def replace_quoted_text(line, text):
 
 
 print("Starting Insertion!")
-for root, _, files in os.walk(folder_path_b):
-    for file in files:
-        file_path = os.path.join(root, file)
-        myfile_path = file_path[len(folder_path_b):]
-        
-        # figure out file ending
-        
-        file_path_base = os.path.join(folder_path_a, myfile_path) 
-        
-        end_index = file_path_base.rfind(".")
-        file_index = file_path_base.rfind('\\')
-        #file_index, path[:file_index], path[file_index+1:end_index+1]
 
-        mylist = os.listdir(file_path_base[:file_index])
-        r = re.compile(file_path_base[file_index+1:end_index+1] + "*")
-        myfile = list(filter(r.match, mylist))[0]
-        
-        if myfile.rfind(".") == -1 and "\\maps\\" in myfile_path:
-            myfile = os.path.join(myfile, "scripts.inc")
-        
-        file_path_base = os.path.join(file_path_base[:file_index], myfile)
-        
-        print(f"================= {myfile_path} =================")
-        
-        if "ipynb" not in myfile_path:
-            with open(file_path_base, 'r', encoding='utf-8') as input_file:
-                content = [line for line in input_file.readlines()]
-            
-            with open(file_path, 'r', encoding='utf-8') as input_file:
-                new_content = [extract_quoted_text(line) for line in input_file.readlines()]
-                new_content = [c for c in new_content if c[0] != -1]
-                
-                j+=1
-                for i, t in new_content:
-                    n = i-1
-                    if j < 0:
-                        print(i, "a: " + content[n].replace("\n", ""), "b: " + t.replace("\n", ""))
-                        print(replace_quoted_text(content[n], t))
-                    content[n] = replace_quoted_text(content[n], t)
-                    
-                    
-                #print("\n\n")
-                
-                #if i < 30:
-                #    print(f"================= {myfile_path} =================")
-                #    print("\n".join([f"{c[0]}: {c[1]}" for c in content]),"\n\n")
-                
-            
-            with open(file_path_base, 'w', encoding='utf-8') as output_file:
-                #if "Littleroot" in myfile_path:
-                #    print("".join(content))
-                output_file.write("".join(content))
+try :
+    for root, _, files in os.walk(folder_path_b):
+        for file in files:
+            file_path = os.path.join(root, file)
+            myfile_path = file_path[len(folder_path_b):]
 
+            # figure out file ending
+            file_path_base = os.path.join(folder_path_a, myfile_path) 
+
+            end_index = file_path_base.rfind(".")
+            file_index = file_path_base.rfind(os.sep)
+
+            mylist = os.listdir(file_path_base[:file_index])
+            r = re.compile(file_path_base[file_index+1:end_index+1] + "*")
+            myfile = list(filter(r.match, mylist))[0]
+
+            if myfile.rfind(".") == -1 and "maps" in myfile_path:
+                myfile = os.path.join(myfile, "scripts.inc")
+
+            file_path_base = os.path.join(file_path_base[:file_index], myfile)
+
+            print(f"================= {myfile_path} =================")
+
+            if "ipynb" not in myfile_path:
+                with open(file_path_base, 'r', encoding='utf-8') as input_file:
+                    content = [line for line in input_file.readlines()]
+
+                with open(file_path, 'r', encoding='utf-8') as input_file:
+                    new_content = [extract_quoted_text(line) for line in input_file.readlines()]
+                    new_content = [c for c in new_content if c[0] != -1]
+
+                    j+=1
+                    for i, t in new_content:
+                        n = i-1
+                        if j < 0:
+                            print(i, "a: " + content[n].replace("\n", ""), "b: " + t.replace("\n", ""))
+                            print(replace_quoted_text(content[n], t))
+                        content[n] = replace_quoted_text(content[n], t)
+
+
+                    #print("\n\n")
+
+                    #if i < 30:
+                    #    print(f"================= {myfile_path} =================")
+                    #    print("\n".join([f"{c[0]}: {c[1]}" for c in content]),"\n\n")
+
+
+                with open(file_path_base, 'w', encoding='utf-8') as output_file:
+                    #if "Littleroot" in myfile_path:
+                    #    print("".join(content))
+                    output_file.write("".join(content))
+    
+except Exception as e:
+    print(e)
+    
 print("Insertion Done!")
